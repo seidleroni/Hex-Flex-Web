cb1fc5c02b6a629e8783bc12aea9469429ecaefd
Jon Seidmann <jons@fractyl.com>
2025-07-16 07:17:22 -0400

    + fixing up/down icons so they are aligned.

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -43,12 +43,12 @@ export const SearchIcon: React.FC<IconProps> = ({ className }) => (
 
 export const GoToPreviousIcon: React.FC<IconProps> = ({ className }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M5 10h14M12 5l-7 5 7 5" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 7l-7 5 7 5" />
     </svg>
 );
 
 export const GoToNextIcon: React.FC<IconProps> = ({ className }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19 14H5M12 19l7-5-7-5" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 17l7-5-7-5" />
     </svg>
 );
